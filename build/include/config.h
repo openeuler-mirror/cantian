c1223f1372d268eeaca80159b48ad72af23fc6d9
lys <120677277@qq.com>
2024-06-14 19:50:22 +0800

    rollback no use file

--- build/include/config.h
+++ build/include/config.h
@@ -1,13 +1,14 @@
 #define VERSION_DESCRIP Cantian
+#define VERSION_MYSQLIP Mysql
 #define DRIVER_VERSION_DESCRIP Cantian
 #define CTSQL_VERSION_DESCRIP 
 #define PACK_PREFIX Cantian
 #define PROJECT_NAME CantianDB
-#define PROJECT_VERSION 24.06
+#define PROJECT_VERSION 24.0.0
 
-#define DD_CT_LIB_VERSION static char* str_CT_LIB_VERSION="Cantian Debug 24.06 747e3aa"
-#define DD_CT_DRIVER_VERSION static char* str_CT_DRIVER_VERSION="Cantian Debug c2755f6"
-#define DD_CT_CTSQL_VERSION static char* str_CT_CTSQL_VERSION="Cantian Debug 726d7d9"
+#define DD_CT_LIB_VERSION static char* str_CT_LIB_VERSION="Cantian Debug 24.0.0 "
+#define DD_CT_DRIVER_VERSION static char* str_CT_DRIVER_VERSION="Cantian Debug "
+#define DD_CT_CTSQL_VERSION static char* str_CT_CTSQL_VERSION="Cantian Debug "
 #define DD_GETDBVERSION cantiand_get_dbversion
 #define DD_GETENCRYVERSION ctencrypt_getDBVersion
 #define DD_CT_LIB_VER_APINAME ctconn_get_dbversion
