676fc68b0450d68a4e6e2ead78aa15b9b45d7850
Duanshaohua <duanshaohua2@huawei.com>
2024-09-06 11:08:50 +0800

    cantian容器绑核, numa内存节点绑定
    cantian容器绑核, numa内存节点绑定

--- pkg/deploy/action/docker/cantian_numa.py
+++ pkg/deploy/action/docker/cantian_numa.py
@@ -12,12 +12,13 @@ from logic.common_func import exec_popen
 from delete_unready_pod import KubernetesService, get_pod_name_from_info
 from om_log import LOGGER as LOG
 
-NUMA_INFO_PATH = "/home/mfdb_core/NUMA-INFO/numa-pod.json"
+NUMA_INFO_PATH = "/root/.kube/NUMA-INFO/numa-pod.json"
 TIME_OUT = 100
 
 
 class LockFile:
     """持锁状态下对文件标识符进行修改，使用阻塞模式等待锁释放"""
+
     @staticmethod
     def lock(handle):
         fcntl.flock(handle, fcntl.LOCK_EX)
@@ -96,8 +97,30 @@ class CPUAllocator:
         return total_cpus, numa_nodes, cpu_info
 
     def bind_cpu(self, cpus, pid=1):
-        cmd = f"taskset -cp {','.join(map(str, cpus))} {pid}"
-        return self.execute_cmd(cmd)
+        cpu_range = ",".join(map(str, cpus))
+        taskset_cmd = f"taskset -cp {cpu_range} {pid}"
+        cpuset_cpu_cmd = f"echo {cpu_range} > /sys/fs/cgroup/cpuset/cpuset.cpus"
+
+        numa_nodes = self.get_numa_nodes_for_cpus(cpus)
+        cpuset_mems_cmd = f"echo {numa_nodes} > /sys/fs/cgroup/cpuset/cpuset.mems"
+
+        self.execute_cmd(taskset_cmd)
+        self.execute_cmd(cpuset_cpu_cmd)
+        self.execute_cmd(cpuset_mems_cmd)
+
+    def get_numa_nodes_for_cpus(self, cpus):
+        """
+        根据 CPU 列表确定属于哪个 NUMA 节点，跨 NUMA 节点时返回对应的 NUMA 节点范围。
+        """
+        total_cpus, numa_nodes, cpu_info = self.get_numa_info()
+
+        nodes = set()
+        for cpu in cpus:
+            for node, info in cpu_info.items():
+                if info["min_cpu"] <= cpu <= info["max_cpu"]:
+                    nodes.add(node)
+
+        return ",".join(sorted(nodes))
 
     def verify_binding(self, expected_cpu_num, pid=1):
         cmd = f"taskset -cp {pid}"
@@ -249,6 +272,7 @@ class CPUAllocator:
                 }
                 self.update_available_cpus(numa_data, cpu_info, binding_cpus)
             else:
+                LOG.error(f"NUMA binding failed for host {hostname}.")
                 numa_data[hostname] = {
                     "bind_cpus": "",
                     "bind_flag": False,
@@ -285,6 +309,10 @@ def load_or_initialize_json(filepath):
     """
     加载或初始化 JSON 文件，并使用文件锁保护文件操作
     """
+    directory = os.path.dirname(filepath)
+    if not os.path.exists(directory):
+        os.makedirs(directory, exist_ok=True)
+
     fd = os.open(filepath, os.O_RDWR | os.O_CREAT, 0o644)
     with os.fdopen(fd, 'r+') as file:
         LockFile.lock(file)
@@ -296,6 +324,7 @@ def load_or_initialize_json(filepath):
                 return {}
         finally:
             LockFile.unlock(file)
+
 
 def write_json_to_file(filepath, data):
     """
